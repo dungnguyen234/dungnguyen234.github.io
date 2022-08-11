08420747509e44a09317c6d551b449313b0ed02a
dungnguyen234 <dung.ntk234@gmail.com>
2022-08-11 16:09:59 +0700

    edit feature set model

--- script.js
+++ script.js
@@ -29,40 +29,45 @@ function staticLoadPlaces() {
 }
 
 var models = [
-    {
-        url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
-        info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0',
-    },
+    // {
+    //     url: './assets/magnemite/scene.gltf',
+    //     scale: '0.5 0.5 0.5',
+    //     info: 'Magnemite, Lv. 5, HP 10/10',
+    //     rotation: '0 180 0',
+    // },
     {
         url: './assets/articuno/scene.gltf',
         scale: '0.2 0.2 0.2',
         rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
-    
+    // {
+    //     url: './assets/Sumerian/scene.gltf',
+    //     scale: '0.8 0.8 0.8',
+    //     rotation: '0 180 0',
+    //     info: 'Sumerian, Lv. 99, HP 150/150',
+    // },
 ];
 
 var modelIndex = 0;
-var setModel = function (model, entity) {
-    if (model.scale) {
-        entity.setAttribute('scale', model.scale);
-    }
+// var setModel = function (model, entity) {
+//     if (model.scale) {
+//         entity.setAttribute('scale', model.scale);
+//     }
 
-    if (model.rotation) {
-        entity.setAttribute('rotation', model.rotation);
-    }
+//     if (model.rotation) {
+//         entity.setAttribute('rotation', model.rotation);
+//     }
 
-    if (model.position) {
-        entity.setAttribute('position', model.position);
-    }
+//     if (model.position) {
+//         entity.setAttribute('position', model.position);
+//     }
 
-    entity.setAttribute('gltf-model', model.url);
+//     entity.setAttribute('gltf-model', model.url);
 
-    const div = document.querySelector('.instructions');
-    div.innerText = model.info;
-};
+//     const div = document.querySelector('.instructions');
+//     div.innerText = model.info;
+// };
 
 function renderPlaces(places) {
     let scene = document.querySelector('a-scene');
