e7ab0872b8c8812a45f993fe9a899f9cee0060e8
dungnguyen234 <dung.ntk234@gmail.com>
2022-08-11 16:34:06 +0700

    change scale of model

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ var models = [
         url: './assets/magnemite/scene.gltf',
         scale: '0.5 0.5 0.5',
         info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0',
+        rotation: '0 0 0',
     },
     {
         url: './assets/articuno/scene.gltf',
