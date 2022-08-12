f312c7d392aafcd0d5397fe28b626d6a21dd3f2c
dungnguyen234 <dung.ntk234@gmail.com>
2022-08-12 08:26:09 +0700

    update pos follow user

--- script.js
+++ script.js
@@ -17,13 +17,15 @@ window.onload = () => {
   
   function renderPlaces(places) {
     let scene = document.querySelector('a-scene');
-  
+   
     places.forEach((place) => {
-      let latitude = place.coords.latitude;
-      let longitude = place.coords.longitude;
-  
+    //   let latitude = place.coords.latitude;
+    //   let longitude = place.coords.longitude;
+    navigator.geolocation.getCurrentPosition((position) => {
+        doSomething(position.coords.latitude, position.coords.longitude);
+      });
       let model = document.createElement('a-entity');
-      model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+      model.setAttribute('gps-entity-place', `latitude: position.coords.latitude; longitude: osition.coords.longitude;`);
       model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
       model.setAttribute('rotation', '0 180 0');
       model.setAttribute('animation-mixer', '');
