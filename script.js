e0e8228ad2904a1eb36078312620f851c2b57957
dungnguyen <dung.ntk234@gmail.com>
2022-08-13 18:27:44 +0700

    revert code

--- script.js
+++ script.js
@@ -20,23 +20,16 @@ window.onload = () => {
     let scene = document.querySelector('a-scene');
   
     places.forEach((place) => {
-      let lat ;
-      let long ;
-      let model = document.createElement('a-entity');
-
-      navigator.geolocation.getCurrentPosition((place) => {
-        lat = place.coords.latitude;
-        long = place.coords.longitude;
-        console.log(place.coords.accuracy);
-        model.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long};`);
-        model.setAttribute('gltf-model', './assets/HP-glass-5.glb');
-        model.setAttribute('rotation', '0 180 0');
-        model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.2 0.2 0.2');
-      });
+      let latitude = place.location.lat;
+      let longitude = place.location.lng;
   
-     
-      // model.setAttribute('position',model.position);
+      let model = document.createElement('a-entity');
+    //   model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+      model.setAttribute('gltf-model', './assets/HP-glass-5.glb');
+      model.setAttribute('rotation', '0 180 0');
+      model.setAttribute('animation-mixer', '');
+      model.setAttribute('scale', '0.2 0.2 0.2');
+      model.setAttribute('position',model.position);
   
       model.addEventListener('loaded', () => {
         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
