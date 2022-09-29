0b0cb441257b648ff49493878fbd884d2c63fcc6
dungnguyen234 <dung.ntk234@gmail.com>
2022-09-30 01:57:22 +0700

    update content of videoar

--- videoAR/main.js
+++ videoAR/main.js
@@ -1,4 +1,7 @@
-import {loadGLTF, loadVideo } from "./libs/loader.js";
+import {loadGLTF, loadVideo ,loadTextures} from "./libs/loader.js";
+import { CSS3DObject } from './libs/three.js-r132/examples/jsm/renderers/CSS3DRenderer.js';
+
+
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const video = await loadVideo("../../assets/videos/sintel/sintel.mp4");
     const texture = new THREE.VideoTexture(video);
-
+    
     const geometry = new THREE.PlaneGeometry(1, 204/480);
     const material = new THREE.MeshBasicMaterial({map: texture});
     const plane = new THREE.Mesh(geometry, material);
@@ -31,58 +34,112 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     //------------------ decalre question item------------------------
     const [
-      cardTexture,
-      emailTexture,
-      locationTexture,
-      webTexture,
-      profileTexture,
+      QuestionTexture,
+      ans1Texture,
+      ans2Texture,
+      ans3Texture,
+      ans4Texture,
     ] = await loadTextures([
       '../../assets/targets/card.png',
       '../../assets/portfolio/icons/email.png',
       '../../assets/portfolio/icons/location.png',
       '../../assets/portfolio/icons/web.png',
       '../../assets/portfolio/icons/profile.png',
-    ])
-    const planeGeometry = new THREE.PlaneGeometry(1, 0.552);
-    const cardMaterial = new THREE.MeshBasicMaterial({map: cardTexture});
-    const card = new THREE.Mesh(planeGeometry, cardMaterial);
+    ]);
+    const PlaneGeometry = new THREE.PlaneGeometry(1, 0.552);
+    const QuestionMaterial = new THREE.MeshBasicMaterial({map: QuestionTexture});
+    const Question = new THREE.Mesh(PlaneGeometry, QuestionMaterial);
 
-    const iconGeometry = new THREE.CircleGeometry(0.075, 32);
-    const emailMaterial = new THREE.MeshBasicMaterial({map: emailTexture});
-    const webMaterial = new THREE.MeshBasicMaterial({map: webTexture});
-    const profileMaterial = new THREE.MeshBasicMaterial({map: profileTexture});
-    const locationMaterial = new THREE.MeshBasicMaterial({map: locationTexture});
+//----------- declare for answer
+    const textElement = document.createElement("div");
+    const textObj = new CSS3DObject(textElement);
+    textObj.position.set(0, -2, 0);
+    textObj.visible = true;
+    textElement.style.background = "#FFFFFF";
+    textElement.style.padding = "30px";
+    textElement.style.fontSize = "60px";
+    const cssAnchor = mindarThree.addCSSAnchor(0);
+    cssAnchor.group.add(textObj);
+    //------------ decalre for question content
 
-    const emailIcon = new THREE.Mesh(iconGeometry, emailMaterial);
-    const webIcon = new THREE.Mesh(iconGeometry, webMaterial);
-    const profileIcon = new THREE.Mesh(iconGeometry, profileMaterial);
-    const locationIcon = new THREE.Mesh(iconGeometry, locationMaterial);
 
-    profileIcon.position.set(-0.42, -0.5, 0);
-    webIcon.position.set(-0.14, -0.5, 0);
-    emailIcon.position.set(0.14, -0.5, 0);
-    locationIcon.position.set(0.42, -0.5, 0);
+    const AnswerGeometry = new THREE.CircleGeometry(0.075, 32);
+    const ans1Material = new THREE.MeshBasicMaterial({map: ans1Texture});
+    const ans2Material = new THREE.MeshBasicMaterial({map: ans2Texture});
+    const ans3Material = new THREE.MeshBasicMaterial({map: ans3Texture});
+    const ans4Material = new THREE.MeshBasicMaterial({map: ans4Texture});
+
+    const ans1Icon = new THREE.Mesh(AnswerGeometry, ans1Material);
+    const ans2Icon = new THREE.Mesh(AnswerGeometry, ans2Material);
+    const ans3Icon = new THREE.Mesh(AnswerGeometry, ans3Material);
+    const ans4Icon = new THREE.Mesh(AnswerGeometry, ans4Material);
+
+    ans1Icon.position.set(-0.42, -0.5, 0);
+    ans2Icon.position.set(-0.14, -0.5, 0);
+    ans3Icon.position.set(0.14, -0.5, 0);
+    ans4Icon.position.set(0.42, -0.5, 0);
     
-    anchor.group.add(card);
-    anchor.group.add(emailIcon);
-    anchor.group.add(webIcon);
-    anchor.group.add(profileIcon);
-    anchor.group.add(locationIcon);
-    card.visible = false;
-    emailIcon.visible = false;
-    webIcon.visible = false;
-    profileIcon.visible = false;
-    locationIcon.visible = false;
+    anchor.group.add(Question);
+    anchor.group.add(ans1Icon);
+    anchor.group.add(ans2Icon);
+    anchor.group.add(ans3Icon);
+    anchor.group.add(ans4Icon);
+
+    ans1Icon.userData.clickable = true;
+    ans2Icon.userData.clickable = true;
+    ans3Icon.userData.clickable = true;
+    ans4Icon.userData.clickable = true;
+
+    //--------------------hide icon at start--------------
+    Question.visible = false;
+    ans1Icon.visible = false;
+    ans2Icon.visible = false;
+    ans3Icon.visible = false;
+    ans4Icon.visible = false;
+
+
+
     video.addEventListener('ended', (event) => {
       
       
-      card.visible = true;
-      emailIcon.visible = true;
-      webIcon.visible = true;
-      profileIcon.visible = true;
-      locationIcon.visible = true;
+      Question.visible = true;
+      ans1Icon.visible = true;
+      ans2Icon.visible = true;
+      ans3Icon.visible = true;
+      ans4Icon.visible = true;
             
         
+    });
+    //------------- handle click---------------------
+    document.body.addEventListener('click', (e) => {
+      const mouseX = (e.clientX / window.innerWidth) * 2 - 1;
+      const mouseY = -(e.clientY / window.innerHeight) * 2 + 1;
+      const mouse = new THREE.Vector2(mouseX, mouseY);
+      const raycaster = new THREE.Raycaster();
+      raycaster.setFromCamera(mouse, camera);
+      const intersects = raycaster.intersectObjects(scene.children, true);
+
+      if (intersects.length > 0) {
+	let o = intersects[0].object; 
+	while (o.parent && !o.userData.clickable) {
+	  o = o.parent;
+	}
+	if (o.userData.clickable) {
+	  if (o === ans1Icon) {
+	    textObj.visible = true;
+	    textElement.innerHTML = "https://github.com/hiukim/mind-ar-js";
+	  } else if (o === ans2Icon) {
+	    textObj.visible = true;
+	    textElement.innerHTML = "hiukim528 [at] gmail";
+	  } else if (o === ans3Icon) {
+	    textObj.visible = true;
+	    textElement.innerHTML = "https://hiukim.com";
+	  } else if (o === ans4Icon) {
+	    textObj.visible = true;
+	    textElement.innerHTML = "Vancouver, Canada";
+	  }
+	}
+      }
     });
     
 
